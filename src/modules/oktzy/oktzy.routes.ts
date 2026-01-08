671b59e3ded80747eb45b7a346c7876d63a79dcc
Sizimon <siizmonsus@gmail.com>
2026-01-08 03:07:04 +0100

    FEAT: Implement clip fetch route.

--- src/modules/oktzy/oktzy.routes.ts
+++ src/modules/oktzy/oktzy.routes.ts
@@ -1,13 +1,19 @@
 //Route layer for Oktzy module (defining API endpoints)
 
 import { Router } from 'express';
-import { login, logout, register } from './oktzy.controller.js';
-import { oktzyAuthRateLimiter } from '../../middleware/oktzy/oktzyRateLimiter.js';
-// import { oktzyAuth } from '../../middleware/oktzy/oktzyAuth.js';
+import { login, logout, register, fetchClips } from './oktzy.controller.js';
+import { oktzyAuthRateLimiter, oktzyRateLimiter } from '../../middleware/oktzy/oktzyRateLimiter.js';
+import { oktzyAuth } from '../../middleware/oktzy/oktzyAuth.js';
 
 const router = Router();
 
+
+// AUTH ROUTES
 router.post('/login', oktzyAuthRateLimiter, login);
 router.post('/register', oktzyAuthRateLimiter, register);
 router.post('/logout', oktzyAuthRateLimiter, logout);
+
+
+// CLIP ROUTES
+router.get('/clips', oktzyAuth, oktzyRateLimiter, fetchClips);
 export default router;
