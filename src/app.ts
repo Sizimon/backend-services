7a40bd2c49a3a5ef5546b9072962aefb3dbe1ff6
Sizimon <siizmonsus@gmail.com>
2026-01-12 08:52:35 +0100

    FEAT: Implement cors which allows all traffic from main domain + any subdomains.

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,24 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import oktzyRouter from './modules/oktzy/oktzy.routes.js';
 // import helmet from 'helmet'; 
-// import cors from 'cors';
+import cors from 'cors';
 
 const app = express();
+
+// Cors configuration to allow all subdomains of head domain.
+app.use(cors({
+    origin: function(origin, callback) {
+        // Allow requests with no origin (like mobile apps or curl requests)
+        if (!origin) return callback(null, true);
+        
+        if (origin.match(/^https?:\/\/(.+\.)?szymonsamus\.dev$/)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
+}));
 
 app.use(express.json());
 app.use(cookieParser());
